f9d6d38d5c63f4535d46225cbc0753961f1bed5a
plexon21 <plexon21@bluewin.ch>
2016-08-11 15:22:02 +0200

    started with playsound proxy

--- src/main/java/com/plexon21/AreaSounds/AreaSounds.java
+++ src/main/java/com/plexon21/AreaSounds/AreaSounds.java
@@ -2,17 +2,22 @@ package com.plexon21.AreaSounds;
 
 import java.util.ArrayList;
 
+import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.ShapedRecipe;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class AreaSounds extends JavaPlugin {
+	FileConfiguration config;
 	@Override
 	public void onEnable() {
 		AreaSounds();
-		System.out.println("InfiniteBucket enabled");
+		saveDefaultConfig();
+		config = getConfig();
 	}
 
 	@Override
@@ -20,25 +25,33 @@ public class AreaSounds extends JavaPlugin {
 
 	}
 
-	public static ShapedRecipe infiniteBucketRecipe() {
-		ItemStack infiniteBucket = new ItemStack(Material.WATER_BUCKET);
-		ItemMeta meta = infiniteBucket.getItemMeta();
-		meta.setDisplayName("Infinite Bucket");
-		ArrayList<String> lore = new ArrayList<String>();
-		lore.add("This is an endless ");
-		lore.add("bucket of water.");
-		meta.setLore(lore);
-		infiniteBucket.setItemMeta(meta);
-		ShapedRecipe recipe = new ShapedRecipe(infiniteBucket);
-		recipe.shape(" E ", " W ", " B ");
-		recipe.setIngredient('E', Material.EYE_OF_ENDER);
-		recipe.setIngredient('W', Material.POTION);
-		recipe.setIngredient('B', Material.BUCKET);
-		return recipe;
+	public void playAreaSound(String name, String volume, String pitch, String radius, String x, String y, String z,
+			String loop) {
+		playAreaSound(name, volume, pitch, radius, x, y, z, null);
+	}
+
+	public void playAreaSound(String name, String volume, String pitch, String radius, String x, String y, String z,
+			String loop, String[] players) {
+		// TODO: World in config
+		World adventureWorld = getServer().getWorld(config.getString("WorldName"));
+		float vol = Float.parseFloat(volume);
+		int rad = Integer.parseInt(radius);
+		Location loc = new Location(adventureWorld, Double.parseDouble(x), Double.parseDouble(y),
+				Double.parseDouble(z));
+		Boolean loopSound = Boolean.parseBoolean(loop);
+
+	}
+
+	public void playAreaSound(String name, float volume, float pitch, int radius, Location location, Boolean loop,
+			int duration, String[] players) {
+		if (players == null) {
+
+		} else {
+
+		}
 	}
 
 	public void AreaSounds() {
-		getServer().addRecipe(infiniteBucketRecipe());
-		getServer().getPluginManager().registerEvents(new AreaSoundsListener(getServer()),this);
+		//getServer().getPluginManager().registerEvents(new AreaSoundsListener(getServer()), this);
 	}
 }
